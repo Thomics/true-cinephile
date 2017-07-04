93c9b77f2ef616426ecfb36b0d5426d61826381b
Thomics <contot@gmail.com>
2017-07-04 14:40:00 -0700

    Started using import/export instead of module.exports

--- app/components/App.js
+++ app/components/App.js
@@ -1,36 +1,54 @@
-var React = require('react');
-console.log('outside')
+
+import React, { Component } from 'react';
+import MovieContainer from './MovieContainer';
 
 
-class App extends React.Component {
+export default class App extends Component {
 
 
   constructor(props) {
     super(props)
 
     this.state = {
-      movieID: 489 //Defaulted movie
+      movieID: 489 //Default movie
     }
   }
 
-
+  //fetchApi={this.fetchApi('https://api.themoviedb.org/3/movie/489?api_key=f6d14169d40228dbf6f63c2a7f56ce70')}
   render() {
     return (
-      <h1 fetchApi={this.fetchApi('https://api.themoviedb.org/3/movie/489?api_key=f6d14169d40228dbf6f63c2a7f56ce70')}>Hello world</h1>
+      <div>
+        <MovieContainer>Hello world</MovieContainer>
+      </div>
     );
   }
 
   fetchApi(url) {
-    console.log('inside');
-    fetch(url).then(function(res) {
-      res.json();
-    }).then(function(data) {
+
+    fetch(url).then((res) => res.json()).then((data) => {
       console.log(data);
-    })
+
+      this.setState = ({
+        backdrop: data.backdrop_path,
+        budget: data.budget,
+        genre: data.genres,
+        homepage: data.homepage,
+        imdbId: data.imdb_id,
+        movieID: data.id,
+        original_title: data.original_title,
+        overview: data.overview,
+        poster: data.poster_path,
+        release: data.release_date,
+        revenue: data.revenue,
+        runtime: data.runtime,
+        tagline: data.tagline,
+        vote: data.vote_average,
+        voteCount: data.vote_count
+      });
+
+    });
 
   }
 
 
 }
-
-module.exports = App;
---
+++ app/components/MovieContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+console.log('movie container');
+
+export default class MovieContainer extends Component {
+
+  render() {
+    var data = this.props.data;
+
+    return(
+      <div>data.movieID</div>
+    )
+  }
+
+}
